80e0a97360ea22dd57898c006dc7a459f0d7fac2
Kade Tatkenhorst <kade.tatkenhorst@my.simpson.edu>
2021-04-28 14:54:16 -0500

    puzzle 2

--- src/main/webapp/js/crosswordBrowse.js
+++ src/main/webapp/js/crosswordBrowse.js
@@ -17,5 +17,27 @@ function goToOldCrossword() {
     });
 }
 
+function goToOldCrossword2() {
+    let dataToServer = {id     : "2"};
+    console.log(dataToServer);
+    let url = "api/crossword";
+    $.ajax({
+        type: 'POST',
+        url: url,
+        data: JSON.stringify(dataToServer),
+        success: function(dataFromServer) {
+            console.log(dataFromServer);
+            let result = JSON.parse(dataFromServer)
+            window.location.href = 'http://localhost:8080/Gradle___com_kurtis_project___kurtis_project_1_0_SNAPSHOT_war/crossword_site.html';
+            //window.location.href = 'http://appdemo2-env.eba-ap9ig6v3.us-east-2.elasticbeanstalk.com/crossword_site.html'
+        },
+        contentType: "application/json",
+        dataType: 'text', // Could be JSON or whatever too
+    });
+}
+
 let oldCrosswordButton = $('#oldCrossword');
 oldCrosswordButton.on("click", goToOldCrossword);
+
+let oldCrosswordButton2 = $('#oldCrossword2');
+oldCrosswordButton2.on("click", goToOldCrossword2);
--- src/main/webapp/js/crosswordlanding.js
+++ src/main/webapp/js/crosswordlanding.js
@@ -91,27 +91,6 @@ function submit() {
     });
 }
 
-// function goToOldCrossword() {
-//     let dataToServer = {id     : "1"};
-//     console.log(dataToServer);
-//     let url = "api/crossword";
-//     $.ajax({
-//         type: 'POST',
-//         url: url,
-//         data: JSON.stringify(dataToServer),
-//         success: function(dataFromServer) {
-//             console.log(dataFromServer);
-//             let result = JSON.parse(dataFromServer)
-//             window.location.href = 'http://localhost:8080/Gradle___com_kurtis_project___kurtis_project_1_0_SNAPSHOT_war/crossword_site.html';
-//             //window.location.href = 'http://appdemo2-env.eba-ap9ig6v3.us-east-2.elasticbeanstalk.com/crossword_site.html'
-//         },
-//         contentType: "application/json",
-//         dataType: 'text', // Could be JSON or whatever too
-//     });
-// }
-
 let newSubmitButton = $('#submit');
 newSubmitButton.on("click", submit);
 
-// let oldCrosswordButton = $('#oldCrossword');
-// oldCrosswordButton.on("click", goToOldCrossword);
