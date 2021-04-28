8f7fb9101280c54985846bea40c2ed1a63277bea
Kade Tatkenhorst <kade.tatkenhorst@my.simpson.edu>
2021-04-28 16:48:05 -0500

    links

--- src/main/webapp/js/crosswordBrowse.js
+++ src/main/webapp/js/crosswordBrowse.js
@@ -9,8 +9,8 @@ function goToOldCrossword() {
         success: function(dataFromServer) {
             console.log(dataFromServer);
             let result = JSON.parse(dataFromServer)
-            window.location.href = 'http://localhost:8080/Gradle___com_kurtis_project___kurtis_project_1_0_SNAPSHOT_war/crossword_site.html';
-            //window.location.href = 'http://appdemo2-env.eba-ap9ig6v3.us-east-2.elasticbeanstalk.com/crossword_site.html'
+            //window.location.href = 'http://localhost:8080/Gradle___com_kurtis_project___kurtis_project_1_0_SNAPSHOT_war/crossword_site.html';
+            window.location.href = 'http://crosswordcreators.com/crossword_site.html'
         },
         contentType: "application/json",
         dataType: 'text', // Could be JSON or whatever too
@@ -28,8 +28,8 @@ function goToOldCrossword2() {
         success: function(dataFromServer) {
             console.log(dataFromServer);
             let result = JSON.parse(dataFromServer)
-            window.location.href = 'http://localhost:8080/Gradle___com_kurtis_project___kurtis_project_1_0_SNAPSHOT_war/crossword_site.html';
-            //window.location.href = 'http://appdemo2-env.eba-ap9ig6v3.us-east-2.elasticbeanstalk.com/crossword_site.html'
+            //window.location.href = 'http://localhost:8080/Gradle___com_kurtis_project___kurtis_project_1_0_SNAPSHOT_war/crossword_site.html';
+            window.location.href = 'http://crosswordcreators.com/crossword_site.html'
         },
         contentType: "application/json",
         dataType: 'text', // Could be JSON or whatever too
--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,11 +1,11 @@
 function playCrossword() {
-    window.location.href = 'http://localhost:8080/Gradle___com_kurtis_project___kurtis_project_1_0_SNAPSHOT_war/crossword_browse.html';
-    //window.location.href = 'http://appdemo2-env.eba-ap9ig6v3.us-east-2.elasticbeanstalk.com/crossword_browse.html'
+    //window.location.href = 'http://localhost:8080/Gradle___com_kurtis_project___kurtis_project_1_0_SNAPSHOT_war/crossword_browse.html';
+    window.location.href = 'http://crosswordcreators.com/crossword_browse.html'
 }
 
 function buildCrossword() {
-    window.location.href = 'http://localhost:8080/Gradle___com_kurtis_project___kurtis_project_1_0_SNAPSHOT_war/crossword_landing.html';
-    //window.location.href = 'http://appdemo2-env.eba-ap9ig6v3.us-east-2.elasticbeanstalk.com/crossword_landing.html'
+    //window.location.href = 'http://localhost:8080/Gradle___com_kurtis_project___kurtis_project_1_0_SNAPSHOT_war/crossword_landing.html';
+    window.location.href = 'http://crosswordcreators.com/crossword_landing.html'
 }
 
 let playCrosswordButton = $('#playCrossword');
